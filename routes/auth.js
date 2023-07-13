e3df676167d350a078c6da4afc8a73005afc9e71
TeyeHack <94695816+TeyeHack@users.noreply.github.com>
2023-07-14 07:15:13 +1100

    last update to auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,24 @@
 const express = require('express');
 
 const authController = require('../controllers/auth');
-const loginController = require('../controllers/auth');
+//const loginController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.post("/signup", loginController.register); //ch
-    router.post("/index", loginController.login); //ch
+    router.post("/signup", authController.register); //ch
+    router.post("/index", authController.login); //ch
+    router.post("/adminlogin", authController.adminlogin); //ch
+    router.post("/admindashboard",authController.admindashboard);
+    router.post("/userdashboard",authController.userdashboard);
+   
+   router.post("/filerecords",authController.getRecords);
+
+   router.get("/filerecords",authController.getRecords);
+  router.post("/forgetpass",authController.forgetpass);
+
+// File upload route
+    router.post('/send-email', authController.sendEmailWithAttachment);
+
 
    /* router.get("/forgetpass", (req,res) =>{
         res.render("forgetpass")
