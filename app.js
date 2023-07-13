20ad6c89b356ef8f267b5a2cdab4ad88eb45418e
TeyeHack <94695816+TeyeHack@users.noreply.github.com>
2023-07-14 08:14:27 +1100

    Add files via upload

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
-const express = require("express")
-const mysql = require("mysql")
+const express = require("express");
+const session = require('express-session');
+const mysql = require("mysql");
+const app = express();
+const path = require("path")
+const fileUpload = require('express-fileupload');
 
+const dotenv = require("dotenv")
+
+dotenv.config({ path: './.env'});
+
+
+
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory))
+// parsing of urlencoded bodies as html forms
+app.use(express.urlencoded({ extended : false}));
+//parse json bodies as sent by API CLIENTS
+app.use(express.json());
+    app.set('view engine', 'hbs');
+
+ 
+
+    //db connection to 
 const db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	passord: '',
-	database:'file_server_amalitech',
+	host: process.env.db_host,
+	user: process.env.db_user,
+	passord: process.env.db_pass,
+	database: process.env.database,
 
 });
 db.connect((error) =>{
@@ -14,14 +36,16 @@ db.connect((error) =>{
     }else{
         console.log("Mysql connected ......")
     }
-})
-
-const app = express();
-
-app.get("/", (req,res) => {
-res.send("<h1>Hello </h1>")
+    
 });
-app.listen (3000, ()=>{
+app.use(fileUpload());
+//routes
+app.use('/', require('./routes/pages'));
+app.use('/auth', require('./routes/auth'));
 
-console.log("Server connected on port 3000");
-})
+
+
+app.listen (3001, ()=>{
+
+console.log("Server connected on port 3001");
+});
